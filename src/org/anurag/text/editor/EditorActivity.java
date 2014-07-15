a6023b2873fbcdec07456268cad4acfcc3fd6b38
Anurag <anurag.dev1512@gmail.com>
2014-07-16 00:22:53 +0530

    NO CHANGES....

--- src/org/anurag/text/editor/EditorActivity.java
+++ src/org/anurag/text/editor/EditorActivity.java
@@ -1,9 +1,23 @@
+/**
+ * Copyright(c) 2013 ANURAG 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * 
+ *               anurag.dev1512@gmail.com
+ *
+ */
 package org.anurag.text.editor;
 
-import java.io.BufferedWriter;
-
-
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -133,7 +147,7 @@ public class EditorActivity extends Activity {
 					File file = new File(path.toString());
 					if(!file.canRead()){
 						/*
-						 * READS THE FILE UNDER NON READ DIRECTORY...SYSTEM FILES WITH NO READ PERMISSION
+						 * READS THE FILE UNDER PROTECTED DIRECTORY...SYSTEM FILES WITH NO READ PERMISSION
 						 */
 						BufferedReader re = LinuxShell.execute("cat "+file.getPath());
 						while((read = re.read(buffer,0,1000))!=-1)
@@ -261,7 +275,7 @@ public class EditorActivity extends Activity {
 	public boolean onKeyDown(int key,KeyEvent e){
 		if(key == KeyEvent.KEYCODE_BACK){
 			if(MODIFIED){
-				new SaveOnExit(EditorActivity.this,size.x*4/5);
+				new SaveOnExit(EditorActivity.this,size.x*8/9);
 			}else
 				EditorActivity.this.finish();
 		}
