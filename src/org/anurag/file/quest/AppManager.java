831c6920d55ffcf9742a532952edae2a30e5afe4
Anurag <anurag.dev1512@gmail.com>
2014-07-16 00:28:16 +0530

    UNUSED METHODS REMOVED....

--- src/org/anurag/file/quest/AppManager.java
+++ src/org/anurag/file/quest/AppManager.java
@@ -16,54 +16,32 @@
 
 package org.anurag.file.quest;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
-
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Environment;
-import android.os.Handler;
-import android.os.Message;
 
+
+/**
+ * THIS CLASS GENERATES THE LIST OF USER,SYSTEM AND COMBINATION OF KINDS OF APPS
+ * IN ARRAYLIST<APPLICATIONINFO>
+ * @author anurag
+ *
+ */
 public class AppManager {
-	private String PATH = Environment.getExternalStorageDirectory().getAbsolutePath();
 	public int SHOW_APP = 2;
-	private Message msg;
 	public ArrayList<ApplicationInfo> ba;
 	public ArrayList<ApplicationInfo> no;
-	private ApplicationInfo info;
 	public ArrayList<ApplicationInfo> nList;
 	private Context mContext;
 	private PackageManager nPManager;
 	private int FLAG_UPDATED_SYS_APP = 0x80;
-	private int BACKUP_STARTED = 0;
-	private int BACKUP_FINISHED = 2;
-	private final int BUFFER = 5*8192;
-	private byte[] data = new byte[BUFFER];
-	private int read = 0;
-	private File file;
-	private String BACKUP_LOCATION = PATH + "/File Quest/AppBackup/";
-	private Handler mHandler;
-	public AppManager(Context context , Handler handler){
-		mContext = context;
-		nList = new ArrayList<ApplicationInfo>();
-		nPManager = mContext.getPackageManager();
-		mHandler = new Handler();
-		this.mHandler = handler;
-	}
+	
 	
 	public AppManager(Context context){
 		mContext = context;
@@ -143,259 +121,5 @@ public class AppManager {
 			}
 		}
 		return 0;
-	}
-	
-	
-	
-	/**
-	 * This Function Takes The Backup Of One App At A Time
-	 * @author Anurag
-	 *
-	 */
-	public class BackupOne implements Runnable{
-		 
-		public BackupOne(ApplicationInfo nInfo){
-			info = nInfo;
-			file = new File(BACKUP_LOCATION);
-			File d = new File(PATH + "/File Quest");
-			if(!d.exists()){
-				d.mkdir();
-				file.mkdir();
-			}else{
-				if(!file.exists())
-					file.mkdir();
-			}
-		}
-		@Override
-		public void run() {
-			Message msg;
-			String source = info.sourceDir;
-			PackageInfo packageInfo = null;
-			try {
-				packageInfo = nPManager.getPackageInfo(info.packageName, 0);
-			} catch (NameNotFoundException e1) {
-				e1.printStackTrace();
-			}
-			String out_file = BACKUP_LOCATION + nPManager.getApplicationLabel(info) + "-v"+packageInfo.versionName + ".apk";
-
-			try {
-				msg = new Message();
-				msg.what = BACKUP_STARTED;
-				String[] appInfo = {info.packageName , nPManager.getApplicationLabel(info).toString()};
-				msg.obj = appInfo;
-				mHandler.sendMessage(msg);
-				BufferedInputStream in = new BufferedInputStream(new FileInputStream(source));
-				BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(out_file));
-				while( (read = in.read(data, 0 , BUFFER)) !=-1 )
-					out.write(data, 0, read);
-				out.flush();
-				out.close();
-				in.close();
-				mHandler.sendEmptyMessage(BACKUP_FINISHED);
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			} catch(IOException e){
-				e.printStackTrace();
-			}
-		}
-	}
-	
-	public class BackupAll implements Runnable{
-		public BackupAll(){
-			nList = giveMeAppList();
-			file = new File(BACKUP_LOCATION);
-			File d = new File(PATH + "/File Quest");
-			if(!d.exists()){
-				d.mkdir();
-				file.mkdir();
-			}else{
-				if(!file.exists())
-					file.mkdir();
-			}
-		}
-		@Override
-		public void run() {
-			
-			for(int i = 0 ; i < nList.size() ; ++i){
-				info = nList.get(i);
-				String source = info.sourceDir;
-				PackageInfo packageInfo = null;
-				try {
-					packageInfo = nPManager.getPackageInfo(info.packageName, 0);
-				} catch (NameNotFoundException e1) {
-					e1.printStackTrace();
-				}
-				String outSource = BACKUP_LOCATION + nPManager.getApplicationLabel(info) + "-v" +packageInfo.versionName + ".apk";
-				String[] appInfo = {info.packageName , nPManager.getApplicationLabel(info).toString()};
-				try {
-					msg = new Message();
-					msg.what = BACKUP_STARTED;
-					msg.obj = appInfo;
-					mHandler.sendMessage(msg);
-					BufferedInputStream in = new BufferedInputStream(new FileInputStream(source));
-					BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(outSource));
-					while( (read = in.read(data, 0 , BUFFER)) != -1 )
-						out.write(data, 0 , read);
-						out.flush();
-						out.close();
-						in.close();
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-				}catch(IOException e){
-					e.printStackTrace();
-				}
-			}
-			mHandler.sendEmptyMessage(BACKUP_FINISHED);
-		}
-		
-	}
-	
-	/**
-	 * 
-	 * @author Anurag
-	 *
-	 */
-	public class FlashableZips implements Runnable{
-		ZipOutputStream out;
-		FileOutputStream dest;
-		BufferedInputStream origin;
-		int flashType;
-		/**
-		 * 
-		 * @param context
-		 * @param type 1 for user Apps and other for system apps
-		 * @throws FileNotFoundException
-		 */
-		public FlashableZips(Context context , int type) throws FileNotFoundException {
-			File file = new File(PATH + "/File Quest - Beta");
-			if(!file.exists())
-				file.mkdir();
-			dest = new FileOutputStream(new File(PATH + "/File Quest/Flashable.zip"));
-			out = new ZipOutputStream(dest);
-			origin = null;
-			mContext = context;
-			flashType = type; 
-			nList = get_downloaded_apps();
-		}
-		@Override
-		public void run() {
-			for(int i = 0 ; i<nList.size() ; ++i ){
-				try {
-					String str = nList.get(i).sourceDir;
-					String label = nPManager.getApplicationLabel(nList.get(i)).toString();
-					msg = new Message();
-					msg.what = BACKUP_STARTED;
-					msg.obj = label;
-					mHandler.sendMessage(msg);
-					FileInputStream in =new FileInputStream(str);
-					origin = new BufferedInputStream(in , BUFFER);
-					ZipEntry entry = new ZipEntry( "system/" + label + ".apk");
-					out.putNextEntry(entry);
-					while ( (read = origin.read(data, 0 , BUFFER)) != -1 )
-						out.write(data, 0 , read);
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-				}catch(IOException e){
-					e.printStackTrace();
-				}
-			}
-			try {
-				msg = new Message();
-				msg.what = 1;
-				mHandler.sendMessage(msg);
-				InputStream in;
-				//Copy Script File as per user selection
-				if(flashType == 1)
-					in = (mContext.getAssets().open("users/updater-script"));
-				else 
-					in = (mContext.getAssets().open("system/updater-script"));
-				origin = new BufferedInputStream(in , BUFFER);
-				ZipEntry entry = new ZipEntry( "META-INF/com/google/android/updater-script");
-				out.putNextEntry(entry);
-				while ( (read = origin.read(data, 0 , BUFFER)) != -1 )
-					out.write(data, 0 , read);
-				//Copy Binary File From Assets
-				in = (mContext.getAssets().open("META-INF/com/google/android/update-binary"));
-				origin = new BufferedInputStream(in , BUFFER);
-				entry = new ZipEntry( "META-INF/com/google/android/update-binary");
-				out.putNextEntry(entry);
-				while ( (read = origin.read(data, 0 , BUFFER)) != -1 )
-					out.write(data, 0 , read);
-				out.close();
-				origin.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			mHandler.sendEmptyMessage(BACKUP_FINISHED);
-		}
-		
-	}
-	
-	
-	public class FlashableZip implements Runnable{
-		ZipOutputStream out;
-		BufferedInputStream origin;
-		FileInputStream in;
-		FileOutputStream dest;
-		int flashType = 0;
-		ZipEntry entry;
-		/**
-		 * @param info
-		 * @param context
-		 * @param type
-		 * @throws FileNotFoundException 
-		 */
-		public FlashableZip(String pos , Context context , int type) throws FileNotFoundException {
-			File file = new File(PATH + "/File Quest");
-			if(!file.exists())
-				file.mkdir();
-			dest = new FileOutputStream(new File(PATH + "/File Quest/Flashable.zip"));
-			out = new ZipOutputStream(dest);
-			PackageInfo i = null;
-			try {
-				i = nPManager.getPackageInfo(pos, 0);
-			} catch (NameNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			in = new FileInputStream(i.applicationInfo.sourceDir);
-			entry = new ZipEntry("system/" + nPManager.getApplicationLabel(i.applicationInfo) + ".apk");			origin = new BufferedInputStream(in , BUFFER);
-			mContext = context;
-			flashType = type;
-		}
-
-		@Override
-		public void run() {
-			try{
-				msg = new Message();
-				msg.what = 1;
-				mHandler.sendMessage(msg);
-				out.putNextEntry(entry);
-				while( (read = origin.read(data, 0 , BUFFER)) !=-1)
-					out.write(data, 0 , read);
-				InputStream ip;
-				if(flashType == 1)
-					ip = mContext.getAssets().open("users/updater-script");
-				else 
-					ip = mContext.getAssets().open("system/updater-script");
-				origin = new BufferedInputStream(ip,BUFFER);
-				entry = new ZipEntry("META-INF/com/google/android/updater-script");
-				out.putNextEntry(entry);
-				while((read=origin.read(data, 0, BUFFER))!=-1)
-					out.write(data, 0, read);
-				ip = mContext.getAssets().open("META-INF/com/google/android/update-binary");
-				origin = new BufferedInputStream(ip , BUFFER);
-				entry = new ZipEntry("META-INF/com/google/android/update-binary");
-				out.putNextEntry(entry);
-				while((read = origin.read(data, 0, BUFFER))!=-1)
-					out.write(data, 0, read);
-				out.close();
-				origin.close();
-				mHandler.sendEmptyMessage(BACKUP_FINISHED);
-			}catch(IOException e){
-				e.printStackTrace();
-			}
-			
-		}
 	}
 }
