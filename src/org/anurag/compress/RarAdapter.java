1a1df44274b7390f0f782fcbad908b46ab5bb759
Anurag <anurag.dev1512@gmail.com>
2014-07-30 21:30:50 +0530

    SOME MINOR CHANGES

--- src/org/anurag/compress/RarAdapter.java
+++ src/org/anurag/compress/RarAdapter.java
@@ -18,6 +18,79 @@
  */
 package org.anurag.compress;
 
-public class RarAdapter {
+import java.util.ArrayList;
+import org.anurag.file.quest.R;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+/**
+ * 
+ * @author Anurag
+ *
+ */
+public class RarAdapter extends BaseAdapter{
+
+	RarObj rFile;
+	Context ctx;
+	ArrayList<RarObj> list;
+	LayoutInflater inflater;
+	
+	public RarAdapter(Context cont , ArrayList<RarObj> ls) {
+		// TODO Auto-generated constructor stub
+		ctx = cont;
+		list = ls;
+		inflater = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	}
+	
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return list.size();
+	}
+
+	@Override
+	public Object getItem(int arg0) {
+		// TODO Auto-generated method stub
+		return list.get(arg0);
+	}
+
+	@Override
+	public long getItemId(int arg0) {
+		// TODO Auto-generated method stub
+		return arg0;
+	}
+
+	class Holder{
+		ImageView icon;
+		TextView fName;
+		TextView fType;
+		TextView fSize;
+		CheckBox box;
+	}
+	@Override
+	public View getView(int pos, View convertView, ViewGroup arg2) {
+		// TODO Auto-generated method stub
+		Holder h = new Holder(); 
+		rFile = list.get(pos);
+		if(convertView == null){
+			convertView = inflater.inflate(R.layout.row_list_1, arg2 , false);
+			h.icon = (ImageView)convertView.findViewById(R.id.fileIcon);
+			h.fName = (TextView)convertView.findViewById(R.id.fileName);
+			h.fType = (TextView)convertView.findViewById(R.id.fileType);
+			h.fSize = (TextView)convertView.findViewById(R.id.fileSize);
+			h.box = (CheckBox)convertView.findViewById(R.id.checkbox);
+			h.box.setVisibility(View.GONE);
+			convertView.setTag(h);
+		}else
+			h = (Holder) convertView.getTag();
+		
+		h.fName.setText(rFile.getFileName());
+		return convertView;
+	}
 }
