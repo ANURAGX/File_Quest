5b6113d3e4d712f1f90d1e053908674d4d9836d1
anurag-dev1512 <anurag.dev1512@gmail.com>
2014-08-19 21:38:58 +0530

    new constant added

--- src/org/anurag/file/quest/Constants.java
+++ src/org/anurag/file/quest/Constants.java
@@ -1,17 +1,22 @@
 /**
- * Copyright(c) 2013 ANURAG 
+ * Copyright(c) 2014 DRAWNZER.ORG PROJECTS -> ANURAG
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
+ *      
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * anurag.dev1512@gmail.com
+ *                             
+ *                             anurag.dev1512@gmail.com
  *
  */
+
 package org.anurag.file.quest;
 
 /**
@@ -20,7 +25,7 @@ package org.anurag.file.quest;
  *
  */
 public class Constants {
-
+	public static boolean LOAD_THUMBNAILS=false;
 	public static long GB = 1024*1024*1024;
 	public static long MB = 1024*1024;
 	public static int BUFFER = 1024*1024;
