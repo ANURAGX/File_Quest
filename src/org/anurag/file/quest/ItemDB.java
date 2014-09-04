054a88bb435b816832e65cd1f5399cb8373bb31e
anurag-dev1512 <anurag.dev1512@gmail.com>
2014-09-05 02:20:38 +0530

    itemdb creates or deletes locked items's record in db....

--- src/org/anurag/file/quest/ItemDB.java
+++ src/org/anurag/file/quest/ItemDB.java
@@ -19,7 +19,9 @@
 
 package org.anurag.file.quest;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -45,9 +47,10 @@ public class ItemDB extends SQLiteOpenHelper{
 	public void onCreate(SQLiteDatabase db) {
 		// TODO Auto-generated method stub
 		db.execSQL("CREATE TABLE ITEMS "+
-				   "ID INTEGER PRIMARY KEY,"+
+				   "(ID INTEGER PRIMARY KEY AUTOINCREMENT,"+
+				   "FILEPATH TEXT,"+
 				   "LOCKED INTEGER,"+
-				   "FAV INTEGER");
+				   "FAV INTEGER);");
 	}
 
 	@Override
@@ -55,5 +58,61 @@ public class ItemDB extends SQLiteOpenHelper{
 		// TODO Auto-generated method stub
 		DB.execSQL("DROP TABLE IF EXISTS ITEMS");
 		onCreate(DB);
+	}
+	
+	/**
+	 * 
+	 * @param PATH
+	 * @param locked
+	 * @param fav
+	 */
+	public void insertNodeToLock(String PATH,int locked,int fav){
+		/**
+		 * performing raw query to ensure that we are not making any 
+		 * duplicate record of the items.. 
+		 */		
+		Cursor cursor = getReadableDatabase().rawQuery("SELECT * FROM ITEMS WHERE FILEPATH = ?",
+				new String[]{PATH});
+		if(cursor.getCount()==0){
+			SQLiteDatabase db = this.getWritableDatabase();
+			ContentValues values = new ContentValues();
+			values.put("FILEPATH", PATH);
+			values.put("LOCKED", locked);
+			values.put("FAV", fav);
+			db.insert("ITEMS", null , values);
+		}
+		
+	}
+	
+	/**
+	 * 
+	 * @param Path
+	 * @return
+	 */
+	public boolean deleteLockedNode(String Path){
+		SQLiteDatabase db = this.getWritableDatabase();
+		return db.delete("ITEMS", "FILEPATH = ?", new String[]{Path})>0;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public boolean deleteAllLockedNodes(){
+		SQLiteDatabase db = this.getWritableDatabase();
+		return db.delete("ITEMS", null , null) > 0;
+	}
+	
+	/**
+	 * 
+	 * @param PATH
+	 * @return
+	 */
+	public boolean isLocked(String PATH){
+		Cursor cursor = getReadableDatabase().rawQuery("SELECT * FROM ITEMS WHERE FILEPATH = ?",
+				new String[]{PATH});
+		if(cursor.getCount()==0)
+			return false;
+		return true;
 	}
 }
