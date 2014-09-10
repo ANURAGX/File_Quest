9670e93d94ba0157e7a4af76b582cdbd55a6e287
anurag-dev1512 <anurag.dev1512@gmail.com>
2014-09-11 00:35:31 +0530

    unused import removed...

--- src/org/anurag/file/quest/MasterPassword.java
+++ src/org/anurag/file/quest/MasterPassword.java
@@ -24,7 +24,6 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
