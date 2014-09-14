e1c4c34f97aefdc62608cee418c26734cc7c1389
anurag-dev1512 <anurag.dev1512@gmail.com>
2014-09-14 23:33:02 +0530

    settings prototype established...

--- src/org/anurag/file/quest/Settings.java
+++ src/org/anurag/file/quest/Settings.java
@@ -20,14 +20,108 @@
 
 package org.anurag.file.quest;
 
+import java.util.ArrayList;
+
+import org.ultimate.menuItems.Info;
+
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ExpandableListView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class Settings extends Activity {
 
+	ExpandableListView folderLs;
+	ExpandableListView uiLs;
+	ArrayList<String> parentAnimList;
+	SettingsAdapter uiAdapter;
+	ArrayList<String> parentFolderList;
+	SettingsAdapter folderAdapter;
+	ListView ls;
+	ArrayList<String> abtString;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.settings_ui);
+		
+		ls = (ListView)findViewById(R.id.abtls);
+		uiLs = (ExpandableListView)findViewById(R.id.intUI);
+		folderLs = (ExpandableListView)findViewById(R.id.folderls);
+		parentAnimList = new ArrayList<String>();
+		parentFolderList = new ArrayList<String>();
+		abtString = new ArrayList<String>();
+		abtString.add(getString(R.string.abtme));
+		
+		
+		parentAnimList.add(getString(R.string.animation));
+		parentAnimList.add(getString(R.string.appearance));
+		uiAdapter = new SettingsAdapter(Settings.this, parentAnimList,0);
+		uiLs.setAdapter(uiAdapter);
+		
+		parentFolderList.add(getString(R.string.startup));
+		parentFolderList.add(getString(R.string.locker));
+		parentFolderList.add(getString(R.string.folderopt));
+		parentFolderList.add(getString(R.string.sethomdir));
+		parentFolderList.add(getString(R.string.cleargesturedata));
+		folderAdapter = new SettingsAdapter(Settings.this, parentFolderList,1);
+		folderLs.setAdapter(folderAdapter);
+		
+		ls.setAdapter(new abtAdapter());
+		ls.setOnItemLongClickListener(null);
+		ls.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,long arg3) {
+				// TODO Auto-generated method stub
+				new Info(Settings.this, FileQuest.size.x*8/9);
+			}
+		});
+	}
+	
+	
+	class abtAdapter extends BaseAdapter{		
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return abtString.size();
+		}
+		@Override
+		public Object getItem(int arg0) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		@Override
+		public long getItemId(int arg0) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+		class abt{
+			ImageView img;
+			TextView txt;
+		}		
+		@Override
+		public View getView(int arg0, View arg1, ViewGroup arg2) {
+			// TODO Auto-generated method stub
+			abt a = new abt();
+			if(arg1==null){
+				LayoutInflater inf = (LayoutInflater) Settings.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE); 
+				arg1 = inf.inflate(R.layout.row_list_expandable, arg2 , false);
+				a.img = (ImageView)arg1.findViewById(R.id.iconImage2);
+				a.txt = (TextView)arg1.findViewById(R.id.directoryName2);
+				arg1.setTag(a);
+			}else
+				a = (abt) arg1.getTag();
+			a.img.setImageDrawable(getResources().getDrawable(R.drawable.ic_launcher_info));
+			a.txt.setText(abtString.get(arg0));
+			return arg1;
+		}		
 	}
 }
