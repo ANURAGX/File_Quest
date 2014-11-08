03f88df849ad1f6cfb491b3e6f16b833a70305b5
anurag-dev1512 <anurag.dev1512@gmail.com>
2014-11-08 14:49:06 +0530

    graphical stats extended by adding pie chart to it....

--- src/org/anurag/file/quest/GraphAnalysis.java
+++ src/org/anurag/file/quest/GraphAnalysis.java
@@ -21,63 +21,210 @@ package org.anurag.file.quest;
 
 import java.util.ArrayList;
 
-import android.app.Activity;
+import android.app.ActionBar;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import com.astuetz.PagerSlidingTabStrip;
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.utils.Legend;
 import com.github.mikephil.charting.utils.XLabels;
+import com.github.mikephil.charting.utils.Legend.LegendPosition;
 import com.github.mikephil.charting.utils.XLabels.XLabelPosition;
 
-public class GraphAnalysis extends Activity{
+public class GraphAnalysis extends FragmentActivity{
 	
+	private PieChart pChart;
 	private BarChart mChart;
-
+	private ActionBar actionBar;
+	private ViewPager pager; 
+	private PagerSlidingTabStrip strip;
+	private PagerFragmentAdapter adpt;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.graph_analysis);
 		
-		mChart = (BarChart)findViewById(R.id.chart1);
-		mChart.setDrawYValues(false);
+		actionBar = getActionBar();
+		actionBar.setTitle(R.string.graph_stats);
+		actionBar.setIcon(R.drawable.analysis);
+		actionBar.setBackgroundDrawable(new ColorDrawable(0x00000000));
 		
-		mChart.setUnit(" MB");
-        mChart.setDescription("");
-        
-        mChart.setDrawYValues(true);
-
-        // if more than 60 entries are displayed in the chart, no values will be
-        // drawn
-        mChart.setMaxVisibleValueCount(60);
-
-        // disable 3D
-        mChart.set3DEnabled(false);
-        // scaling can now only be done on x- and y-axis separately
-        mChart.setPinchZoom(false);
-
-        mChart.setDrawBarShadow(false);
-        
-        mChart.setDrawVerticalGrid(false);
-        mChart.setDrawHorizontalGrid(false);
-        mChart.setDrawGridBackground(false);
-
-        XLabels xLabels = mChart.getXLabels();
-        xLabels.setPosition(XLabelPosition.BOTTOM);
-        xLabels.setCenterXLabelText(true);
-        xLabels.setSpaceBetweenLabels(0);
-
-        mChart.setDrawYLabels(false);
-        mChart.setDrawLegend(false);
-        // add a nice and smooth animation
-        setGraphData();
-        
-        mChart.animateY(2500);
+		
+		pager = (ViewPager)findViewById(R.id.graph_pager);
+		strip = (PagerSlidingTabStrip)findViewById(R.id.graph_ind);
+		adpt = new PagerFragmentAdapter(getSupportFragmentManager());
+		pager.setAdapter(adpt);
+		strip.setViewPager(pager);
 	}
 
+	/**
+	 * 
+	 * @author Anurag....
+	 *
+	 */
+	private class PagerFragmentAdapter extends FragmentPagerAdapter{
+		String[] titles = getResources().getStringArray(R.array.graph_titles);
+		
+		public PagerFragmentAdapter(FragmentManager fm) {
+			super(fm);
+			// TODO Auto-generated constructor stub
+		}
+
+		@Override
+		public Fragment getItem(int arg0) {
+			// TODO Auto-generated method stub
+			switch(arg0){
+			case 0:
+					return new BarFragment();
+			default:
+					return new PieFragment();
+			}
+		}		
+		@Override
+		public CharSequence getPageTitle(int position) {
+			// TODO Auto-generated method stub
+			return titles[position];
+		}
+
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return titles.length;
+		}		
+	}
+
+	/**
+	 * 
+	 * @author Anurag....
+	 *
+	 */
+	private class BarFragment extends Fragment{ 
+		public BarFragment() {
+			// TODO Auto-generated constructor stub
+		}
+		
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+			// TODO Auto-generated method stub
+			View view = inflater.inflate(R.layout.bar_graph_analysis_fragment, container , false);
+			return view;
+		}
+
+		@Override
+		public void onViewCreated(View view, Bundle savedInstanceState) {
+			// TODO Auto-generated method stub
+			mChart = (BarChart)view.findViewById(R.id.chart1);
+			mChart.setDrawYValues(false);
+			
+			mChart.setUnit(" MB");
+	        mChart.setDescription("");
+	        
+	        mChart.setDrawYValues(true);
+
+	        // if more than 60 entries are displayed in the chart, no values will be
+	        // drawn
+	        mChart.setMaxVisibleValueCount(60);
+
+	        // disable 3D
+	        mChart.set3DEnabled(false);
+	        // scaling can now only be done on x- and y-axis separately
+	        mChart.setPinchZoom(false);
+
+	        mChart.setDrawBarShadow(false);
+	        
+	        mChart.setDrawVerticalGrid(false);
+	        mChart.setDrawHorizontalGrid(false);
+	        mChart.setDrawGridBackground(false);
+
+	        XLabels xLabels = mChart.getXLabels();
+	        xLabels.setPosition(XLabelPosition.BOTTOM);
+	        xLabels.setCenterXLabelText(true);
+	        xLabels.setSpaceBetweenLabels(0);
+
+	        mChart.setDrawYLabels(false);
+	        mChart.setDrawLegend(false);
+	        // add a nice and smooth animation
+	        setGraphData();
+	        
+	        mChart.animateY(2500);
+		}		
+	}
+	
+	private class PieFragment extends Fragment{
+		public PieFragment() {
+			// TODO Auto-generated constructor stub
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+			// TODO Auto-generated method stub
+			return inflater.inflate(R.layout.pie_chart_analysis, container , false);
+		}
+
+		@Override
+		public void onViewCreated(View view, Bundle savedInstanceState) {
+			// TODO Auto-generated method stub
+			pChart = (PieChart)view.findViewById(R.id.chart2);
+			
+			// change the color of the center-hole
+	        pChart.setHoleColor(Color.rgb(235, 235, 235));
+
+	        pChart.setHoleRadius(40f);
+	        pChart.setDescription("");
+
+	        pChart.setDrawYValues(true);
+	        pChart.setDrawCenterText(true);
+
+	        pChart.setDrawHoleEnabled(true);
+
+	        pChart.setRotationAngle(0);
+
+	        // draws the corresponding description value into the slice
+	        pChart.setDrawXValues(true);
+
+	        // enable rotation of the chart by touch
+	        pChart.setRotationEnabled(true);
+
+	        // display percentage values
+	        pChart.setUsePercentValues(true);
+	        
+	        mChart.setTouchEnabled(false);
+
+	        pChart.setCenterText(getResources().getString(R.string.graph_stats));
+	        
+	        setPieData();
+	        mChart.animateXY(1500, 1500);
+	        Legend l = mChart.getLegend();
+	        l.setPosition(LegendPosition.RIGHT_OF_CHART);
+	        l.setXEntrySpace(7f);
+	        l.setYEntrySpace(5f);
+		}
+		
+	}
+	
+	/**
+	 * function setting the bar graph data....
+	 */
 	private void setGraphData() {
 		// TODO Auto-generated method stub
 		ArrayList<BarEntry> yVal = new ArrayList<BarEntry>();
@@ -99,7 +246,7 @@ public class GraphAnalysis extends Activity{
 		xVal.add("Unknown");
 		
 		BarDataSet set = new BarDataSet(yVal, "Data Set");
-		set.setColors(ColorTemplate.VORDIPLOM_COLORS);
+		set.setColors(ColorTemplate.COLORFUL_COLORS);
 		
 		ArrayList<BarDataSet> sets = new ArrayList<BarDataSet>();
 		sets.add(set);
@@ -109,4 +256,52 @@ public class GraphAnalysis extends Activity{
 		mChart.invalidate();
 	}
 
+	/**
+	 * 
+	 */
+	public void setPieData() {
+		// TODO Auto-generated method stub
+		ArrayList<Entry> yVal = new ArrayList<Entry>();
+		yVal.add(new Entry(Utils.musicsize, 0));
+		yVal.add(new Entry(Utils.apksize, 1));
+		yVal.add(new Entry(Utils.imgsize , 2));
+		yVal.add(new Entry(Utils.vidsize, 3));
+		yVal.add(new Entry(Utils.docsize, 4));
+		yVal.add(new Entry(Utils.zipsize, 5));
+		yVal.add(new Entry(Utils.missize, 6));
+		
+		ArrayList<String> xVal = new ArrayList<String>();
+		xVal.add("Music");
+		xVal.add("Apps");
+		xVal.add("Photos");
+		xVal.add("Videos");
+		xVal.add("Docs");
+		xVal.add("Zips");
+		xVal.add("Unknown");
+		
+		PieDataSet set = new PieDataSet(yVal, "");
+		set.setSliceSpace(4f);
+		
+		// add a lot of colors
+
+        ArrayList<Integer> colors = new ArrayList<Integer>();
+
+
+        for (int c : ColorTemplate.COLORFUL_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.LIBERTY_COLORS)
+            colors.add(c);
+        
+        for (int c : ColorTemplate.PASTEL_COLORS)
+            colors.add(c);
+        
+        colors.add(ColorTemplate.getHoloBlue());
+        
+        set.setColors(colors);
+        PieData data = new PieData(xVal, set);
+        pChart.setData(data);
+        mChart.highlightValues(null);
+        mChart.invalidate();
+	}
 }
