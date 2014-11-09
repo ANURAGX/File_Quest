c566312c2827fca4304f6955ffcd0ba76a20f1a5
anurag-dev1512 <anurag.dev1512@gmail.com>
2014-11-09 13:18:45 +0530

    file properties improved....

--- src/org/anurag/file/quest/FileProperties.java
+++ src/org/anurag/file/quest/FileProperties.java
@@ -21,12 +21,20 @@ package org.anurag.file.quest;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Locale;
 
 import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.media.MediaMetadataRetriever;
 import android.os.Bundle;
 import android.os.Environment;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.BarEntry;
@@ -47,24 +55,44 @@ public class FileProperties extends Activity{
 	private PieChart pChart;
 	private File file;
 	private long size;
+	private String type;
+	private ImageView prp_img;
+	private TextView prp_name;
+	private TextView prp_path;
+	private TextView prp_type;
+	private TextView prp_size;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.file_properties);
 		
+		getActionBar().setIcon(R.drawable.ic_launcher_stats);
 		getActionBar().setBackgroundDrawable(new ColorDrawable(0x00000000));
 		getActionBar().setTitle(R.string.properties);
 		
 		file = new File(getIntent().getStringExtra("path"));
 		size = 0;
 		pChart = (PieChart)findViewById(R.id.chart);
+		prp_img = (ImageView)findViewById(R.id.prop_img);
+		prp_name = (TextView)findViewById(R.id.prp_name);
+		prp_path = (TextView)findViewById(R.id.prp_path);
+		prp_type = (TextView)findViewById(R.id.prp_type);
+		prp_size = (TextView)findViewById(R.id.prp_size);
+		
+		
+		prp_img.setImageDrawable(buildIcon(file));
+		prp_type.setText((getString(R.string.type) + " - " + type).toUpperCase(Locale.ENGLISH));
+		prp_path.setText(getString(R.string.filepath) + " - "+file.getAbsolutePath());
+		prp_name.setText(getString(R.string.filename) + " - "+file.getName());
+	
+		
 		// change the color of the center-hole
         pChart.setHoleColor(Color.rgb(235, 235, 235));
 
         pChart.setHoleRadius(40f);
         pChart.setDescription("");
-
         pChart.setDrawYValues(true);
         pChart.setDrawCenterText(true);
 
@@ -93,15 +121,30 @@ public class FileProperties extends Activity{
         l.setYEntrySpace(5f);
 		
 	}
-	
+	/**
+	 * this function sets the pie chart data....
+	 */
 	private void setPieData() {
 		// TODO Auto-generated method stub
 		ArrayList<Entry> yVal = new ArrayList<Entry>();
 		yVal.add(new BarEntry(Environment.getExternalStorageDirectory().getTotalSpace(), 0));
-		if(file.isFile())
+		if(file.isFile()){
+			if(type.equalsIgnoreCase("MUSIC")){
+				//if type is music then load the album art....
+				MediaMetadataRetriever ret = new MediaMetadataRetriever();
+				ret.setDataSource(file.getPath());
+				byte[] data = ret.getEmbeddedPicture();
+				Bitmap map = BitmapFactory.decodeByteArray(data, 0, data.length);
+				if(map != null)
+					prp_img.setImageBitmap(map);
+			}
 			yVal.add(new BarEntry(file.length(), 1));
-		else if(file.isDirectory())
+			prp_size.setText(getString(R.string.filesize) + " - " + RootManager.getSize(file));
+		}	
+		else if(file.isDirectory()){
 			yVal.add(new BarEntry(calculateLength(), 1));
+			prp_size.setText(Utils.size(size));
+		}	
 		
 		ArrayList<String> xVal = new ArrayList<String>();
 		xVal.add(getResources().getString(R.string.sd));
@@ -152,6 +195,102 @@ public class FileProperties extends Activity{
 					getFileSize(a[i]);
 			}
 		}
+	}
+	
+	/**
+	 * 
+	 * @return icon for the file....
+	 */
+	private Drawable buildIcon(File f){
+		final Resources res = getResources();
+		if(f.isDirectory()){
+			type = getString(R.string.directory);
+			return res.getDrawable(Constants.FOLDERS[Constants.FOLDER_ICON]);
+		}
+		
+		String name = f.getName().toLowerCase(Locale.ENGLISH);
+		
+		if(name.endsWith(".zip")){
+			
+			type = getString(R.string.zip);
+			return res.getDrawable(R.drawable.ic_launcher_zip_it);
+			
+		}else if(name.endsWith(".7z")){
+			
+			type = getString(R.string.zip7);
+			return res.getDrawable(R.drawable.ic_launcher_7zip);
+			
+		}else if(name.endsWith(".rar")){
+			
+			type = getString(R.string.rar);
+			return res.getDrawable(R.drawable.ic_launcher_rar);
+			
+		}else if(name.endsWith(".tar")||name.endsWith(".tar.gz")||name.endsWith(".tar.bz2")){
+			
+			type = getString(R.string.tar);
+			return res.getDrawable(R.drawable.ic_launcher_7zip);
+			
+		}
+		else if(name.endsWith(".mp3")||name.endsWith(".ogg")||name.endsWith(".m4a")||name.endsWith(".wav")
+				||name.endsWith(".amr")){
+			
+			type = getString(R.string.music);
+			return res.getDrawable(R.drawable.ic_launcher_music);
+			
+		}
+		else if(name.endsWith(".apk")){
+			
+			type = getString(R.string.application);
+			return res.getDrawable(R.drawable.ic_launcher_apk);
+			
+		}else if(name.endsWith(".sh")||name.endsWith(".prop")||name.endsWith("init")
+				||name.endsWith(".default")||name.endsWith(".rc")){
+			
+			type = getString(R.string.script);
+			return res.getDrawable(R.drawable.ic_launcher_sh);
+			
+		}else if(name.endsWith(".pdf")){
+			
+			type = getString(R.string.pdf);
+			return res.getDrawable(R.drawable.ic_launcher_adobe);
+			
+		}else if(name.endsWith(".htm")||name.endsWith(".html")||name.endsWith(".mhtml")){
+			
+			type = getString(R.string.web);
+			return res.getDrawable(R.drawable.ic_launcher_web_pages);
+			
+		}else if(name.endsWith(".flv")||name.endsWith(".mp4")||name.endsWith(".3gp")||name.endsWith(".avi")
+				||name.endsWith(".mkv")){
+			
+			type = getString(R.string.vids);
+			return res.getDrawable(R.drawable.ic_launcher_video);
+			
+		}	
+		else if(name.endsWith(".bmp")||name.endsWith(".gif")||name.endsWith(".jpeg")||name.endsWith(".jpg")
+				||name.endsWith(".png")){
+			
+			type = getString(R.string.image);
+			return res.getDrawable(R.drawable.ic_launcher_images);
+			
+		}else if(name.endsWith(".txt")||name.endsWith(".log")||name.endsWith(".ini")){
+			
+			type = getString(R.string.text);
+			return res.getDrawable(R.drawable.ic_launcher_text);
+			
+		}
+		else if(name.endsWith(".doc")||name.endsWith(".ppt")||name.endsWith(".docx")||name.endsWith(".DOC")
+				||name.endsWith(".pptx")||name.endsWith(".csv")){
+			
+			type = getString(R.string.docs);
+			return res.getDrawable(R.drawable.ic_launcher_ppt);
+			
+		}
+		else{
+			
+			type = getString(R.string.unknown);
+			return res.getDrawable(R.drawable.ic_launcher_unknown);
+			
+		}		
 	}
 
 	
