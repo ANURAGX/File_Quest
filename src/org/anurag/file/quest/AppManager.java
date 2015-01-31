a9a3b8e8d77e742f596312e3418b440efac2d414
anurag <anurag.dev1512@gmail.com>
2015-01-31 14:26:56 +0530

    same 6 sorting applied to apps list....

--- src/org/anurag/file/quest/AppManager.java
+++ src/org/anurag/file/quest/AppManager.java
@@ -23,6 +23,7 @@ package org.anurag.file.quest;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
@@ -39,20 +40,18 @@ import android.os.Environment;
  */
 public class AppManager {
 	public int SHOW_APP = 2;
-	public ArrayList<ApplicationInfo> ba;
-	public ArrayList<ApplicationInfo> no;
 	public ArrayList<ApplicationInfo> nList;
 	private Context mContext;
 	private PackageManager nPManager;
 	private int FLAG_UPDATED_SYS_APP = 0x80;
 	
+	private AppUtils utils;
 	
 	public AppManager(Context context){
 		mContext = context;
 		nList = new ArrayList<ApplicationInfo>();
 		nPManager = mContext.getPackageManager();
-		ba = new ArrayList<ApplicationInfo>();
-		no = new ArrayList<ApplicationInfo>();
+		utils = new AppUtils(nPManager);
 	}
 	
 	
@@ -65,6 +64,36 @@ public class AppManager {
 			   (appInfo.flags & FLAG_UPDATED_SYS_APP) == 0 && 
 			   appInfo.flags != 0)
 					nList.add(appInfo);
+		}
+		switch(Constants.SORT_TYPE){
+		case 1:
+			//a-z sort
+			utils.a_zSort(nList);
+			break;
+			
+		case 2:
+			//z-a sort
+			utils.z_aSort(nList);
+			break;
+			
+		case 3:
+			//smaller size first sort....
+			utils.smallSize_Sort(nList);
+			break;
+			
+		case 4:
+			//larger size first sort....
+			utils.bigSize_Sort(nList);
+			break;
+			
+		case 5:
+			//new date file first....
+			utils.newDate_Sort(nList);
+			break;
+			
+		case 6:
+			//old date file first....
+			utils.oldDate_Sort(nList);
 		}
 		return nList;
 	}
