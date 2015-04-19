b0945daca0d4664cb895b52fb2b491d43546e939
Anurag <anuraxSharma1512@gmail.com>
2015-04-19 11:09:04 +0530

    sync

--- File_Quest/src/org/ultimate/menuItems/SetLaunchDir.java
+++ File_Quest/src/org/ultimate/menuItems/SetLaunchDir.java
@@ -31,19 +31,13 @@ import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 @SuppressLint("NewApi")
 public class SetLaunchDir{
